6ed24f3115e6ae3b2a3cc7f2f7be3775f5065383
Matheus Sousa <devlpsousa@gmail.com>
2024-02-28 10:39:49 -0300

    refactor: code refactoring

--- src/components/PaymentMethods/PaymentMethods.jsx
+++ src/components/PaymentMethods/PaymentMethods.jsx
@@ -3,7 +3,7 @@ import "./PaymentMethods.scss";
 export default function PaymentMethod() {
   return (
     <div className="card ">
-      <div className="header">
+      <div className="p-2">
         <div className="image">
           <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
--- src/page/Page.jsx
+++ src/page/Page.jsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <>
       <Header />
-      <img src="/HomeWallpaper.png" alt="mainImage" className="w-full object-cover max-h-[600px] mainImg" />
+      <img src="/HomeWallpaper.png" alt="mainImage" className="w-full object-cover max-h-[600px] mainImg" id="backToTop" />
       <div className="text-center p-2 mb-10 mt-2" id="bots">
         <h1>🌟 Eficiência e segurança. Experimente a inovação da Infinity Store!🏷️🌟</h1>
       </div>
@@ -42,7 +42,7 @@ export default function Page() {
         </div>
       </section>
 
-      <footer className="p-6 px-8 flex flex-wrap justify-between items-center w-full bg-indigo-950 mt-20">
+      <footer className="p-6 px-8 flex flex-wrap justify-between items-center w-full bg-indigo-950 mt-20 z-20">
         <h1 className="font-bold text-2xl">Infinity Store</h1>
         <div className="flex items-center flex-col gap-1">
           <div className="flex items-center gap-1">
@@ -60,6 +60,14 @@ export default function Page() {
           </div>
         </div>
       </footer>
+      <a href="#backToTop">
+        <button className="Btn">
+          <svg height="1.2em" className="arrow" viewBox="0 0 512 512">
+            <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
+          </svg>
+          <p className="text">Voltar para o Topo</p>
+        </button>
+      </a>
     </>
   );
 }
